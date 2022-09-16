3d6c28523f9547e348b3c9f891db53ebbcbfbd65
cbozbuga41@gmail.com <cbozbuga41@gmail.com>
2022-09-16 11:46:53 +0300

    Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title:string = "northwind";
-  user:string = 'Can Bozbuga';
-  product={productId:1, productName:'Bardak', categoryId:1, unitPrice:5}
+  title: string = "northwind";
+  user: string = 'Can Bozbuga';
+  
 }
