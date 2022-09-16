a43e3283fb0165e311f2ae83400e58852abdf5b2
cbozbuga41@gmail.com <cbozbuga41@gmail.com>
2022-09-16 11:40:21 +0300

    17th

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'northwind';
+  title:string = "northwind";
+  user:string = 'Can Bozbuga';
+  product={productId:1, productName:'Bardak', categoryId:1, unitPrice:5}
 }
